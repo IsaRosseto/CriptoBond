2d3dc93949f657af4936f578831aeeda94cd2c6a
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2024-05-24 16:23:31 -0300

    Add files via upload

---
+++ controller/MainController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import view.TelaBoasVindas;
+
+public class MainController {
+
+    public static void main(String[] args) {
+        new TelaBoasVindas().setVisible(true);
+    }
+}
